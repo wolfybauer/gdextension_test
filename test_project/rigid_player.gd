f22403aa403dbac795ef9524c75cd43a78d95721
wolfy <thomas.bauer@astrodigital.com>
2026-01-06 12:18:06 -0800

    improve rigig_player template

--- test_project/rigid_player.gd
+++ test_project/rigid_player.gd
@@ -1,16 +1,19 @@
 extends RigidCharacterBody3D
 
-const JUMP_HEIGHT: float = 1.7
 const ROTATION_SPEED := 6.0 
 
 @export var speed := 8.0
+@export var damp:float = 10.0
+@export var jump_height: float = 2.0
+
 const SMOOTH_ACCELERATION_CURVE = preload("res://smooth_acceleration_curve.tres")
 
 @onready var camera_pivot: Node3D = $CameraPivot
 @onready var camera_3d: Camera3D = $CameraPivot/Camera3D
 
 var direction:Vector3
-var _wants_to_jump: bool = false
+var _wants_to_jump:bool = false
+var _jump_released:bool = false
 
 func _ready() -> void:
 	mass = 60.0
@@ -21,10 +24,14 @@ func _input(event):
 
 	if event.is_action_pressed("ui_accept") and is_on_floor():
 		_wants_to_jump = true
-		var velocity: float = sqrt(2.0 * get_gravity().length() * JUMP_HEIGHT)
+		_jump_released = false
+		var velocity: float = sqrt(2.0 * get_gravity().length() * jump_height)
 		apply_central_impulse(mass * velocity * up_direction)
 	else:
 		_wants_to_jump = false
+	
+	if event.is_action_released("ui_accept"):
+		_jump_released = true
 
 func _physics_process(_delta: float) -> void:
 	linear_damp = 0.0
@@ -36,21 +43,25 @@ func _physics_process(_delta: float) -> void:
 	if direction or _wants_to_jump:
 		target_velocity.x = lerp(target_velocity.x, direction.x * speed, 0.1)
 		target_velocity.z = lerp(target_velocity.z, direction.z * speed, 0.1)
+		
 		##now rotate the model
 		#if direction:
 			#rotate_model(direction, delta)
 			#player_animation_state = animation_state.RUNNING
 	else:
 		if is_on_floor():
-			linear_damp = 30.0
+			linear_damp = damp
 		target_velocity.x = 0.0
 		target_velocity.z = 0.0
 		#player_animation_state = animation_state.IDLE
 	
 	#if not is_on_floor():
 		#player_animation_state = animation_state.JUMPING
-	
-	# _push_away_rigid_bodies()
+	if _jump_released and linear_velocity.dot(up_direction) > 0.0:
+		var lv:Vector3 = linear_velocity
+		lv -= up_direction * linear_velocity.dot(up_direction) * 0.1
+		linear_velocity = lv
+
 	move_and_slide()
 
 func _modify_move_force(move_force:Vector3) -> Vector3:
@@ -60,9 +71,9 @@ func _modify_move_force(move_force:Vector3) -> Vector3:
 	var z_offset: float =((horizontal_velocity.z * basis.z)).normalized().dot(
 			(target_velocity.z * basis.z).normalized())
 	
-	#if x_offset < 0.0:
-	move_force.x *= SMOOTH_ACCELERATION_CURVE.sample(x_offset)
-	#if z_offset < 0.0:
-	move_force.z *= SMOOTH_ACCELERATION_CURVE.sample(z_offset)
+	if x_offset < 0.0:
+		move_force.x *= SMOOTH_ACCELERATION_CURVE.sample(x_offset)
+	if z_offset < 0.0:
+		move_force.z *= SMOOTH_ACCELERATION_CURVE.sample(z_offset)
 	
 	return move_force
